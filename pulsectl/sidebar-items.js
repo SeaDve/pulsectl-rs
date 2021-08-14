c816a4a2b248a0ce0e5b252501b2718b8bbab9a7
SeaDve <SeaDve@users.noreply.github.com>
2021-08-14 11:52:37 +0000

    deploy: d7d831b58ccce39ab34458cd22930c9b6e4e6506

--- pulsectl/sidebar-items.js
+++ pulsectl/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["controllers","Terminologies"]],"struct":[["Handler",""]]});
+initSidebarItems({"enum":[["Error","Error thrown when PulseAudio throws an error code, there are 3 variants"]],"mod":[["controllers","Terminologies"]],"struct":[["Handler",""]]});
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["pulsectl"] = {"name":"","dirs":[{"name":"controllers","files":["errors.rs","mod.rs","types.rs"]}],"files":["errors.rs","lib.rs"]};
+sourcesIndex["pulsectl"] = {"name":"","dirs":[{"name":"controllers","files":["error.rs","mod.rs","types.rs"]}],"files":["error.rs","lib.rs"]};
 createSourceSidebar();
