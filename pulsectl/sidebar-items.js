cac919e449fd66a6fe54b1e70e2386f749a95493
SeaDve <SeaDve@users.noreply.github.com>
2021-08-14 12:56:44 +0000

    deploy: 28aef667cda21ab44ae9a938be1e6ce41a5656e6

--- pulsectl/sidebar-items.js
+++ pulsectl/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error","Error thrown when PulseAudio throws an error code, there are 3 variants"]],"mod":[["controllers","Terminologies"]],"struct":[["Handler",""]]});
+initSidebarItems({"enum":[["Error","Error thrown when PulseAudio throws an error code, there are 4 variants"]],"mod":[["controllers","Terminologies"]],"struct":[["Handler",""]]});
